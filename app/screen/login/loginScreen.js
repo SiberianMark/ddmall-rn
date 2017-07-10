9f8afb0bb1ecaaf061c8f84a010f0571bd78ca46
JASONM <mashaobin911215@gmail.com>
2017-07-10 16:00:48 +0800

    alter login

--- app/screen/login/loginScreen.js
+++ app/screen/login/loginScreen.js
@@ -10,7 +10,9 @@ import {
 	View,
 	WebView,
 	TextInput,
-	Button
+	Button,
+	TouchableHighlight,
+	TouchableOpacity 
 } from "react-native";
 import Styles from "./loginStyle"
 
@@ -18,25 +20,38 @@ export default class loginScreen extends Component{
 	render(){
 		return (
 			<View style={Styles.view_Container}>
-				
-				<View>
-					<TextInput style={Styles.TextInput} placeholder="账户"></TextInput>
-					<TextInput style={Styles.TextInput} placeholder="密码" password={true}></TextInput>
-				</View>
-				<Button 
-					style={Styles.Button}
-					onPress={this.onPressLogin} 
-					title="登录"
-				/>
-				<View style={Styles.moreMenu}>
-					<Text>注册</Text>
-					<Text>忘记密码</Text>
+				<View style={Styles.viewLogin}>
+					<Image style={Styles.Image} source={require('./img/icon.png')}></Image>
+					<View>
+						{/*input默认样式无法修改，跟button一样,password属性无效*/}
+						<TextInput style={Styles.TextInput} underlineColorAndroid="transparent" placeholder={"账户"} />
+						<TextInput style={Styles.TextInput} underlineColorAndroid="transparent" placeholder={"密码"} secureTextEntry={true} />
+					</View>
+					<TouchableOpacity onPress={this.onPressLogin}>
+						<View style={Styles.loginBtn}><Text style={{color:'#fff'}}>登录</Text></View>
+					</TouchableOpacity>
+					<View style={Styles.moreMenu}>
+						<TouchableOpacity onPress={this.onPressRegist}><Text>注册</Text></TouchableOpacity>
+						<TouchableOpacity onPress={this.onPressFindPsw}><Text>忘记密码</Text></TouchableOpacity>
+					</View>
 				</View>	
+				<View style={Styles.otherLogin}>
+						<Text>其他登录方式</Text>
+						<Image style={Styles.otherImges} source={require('./img/icon3.png')}></Image>
+						<Image style={Styles.otherImges} source={require('./img/icon7.png')}></Image>
+						<Image style={Styles.otherImges} source={require('./img/icon8.png')}></Image>
+				</View>
 			</View>
 		)
 
 	}
 	onPressLogin(){
 		alert("press success");
+	}
+	onPressRegist(){
+		alert("press regist");
+	}
+	onPressFindPsw(){
+		alert("press findPsw");
 	}
 }
--- app/screen/login/loginStyle.js
+++ app/screen/login/loginStyle.js
@@ -2,10 +2,58 @@ import {StyleSheet} from 'react-native';
 import {colors} from '../../common/colors'
 
 
+var Dimensions = require('Dimensions');
+var width = Dimensions.get('window').width;
+
 export default StyleSheet.create({
 	view_Container:{
+		flex:1,
+		justifyContent:'space-between'
 	},
 	Image:{
+		marginTop: 100,
+		width:70,
+		height:70,
+		borderRadius:40,
+		alignSelf:'center'
+	},
+	TextInput:{
+		marginTop:20,
+		textAlign:'center',
+		borderBottomWidth:1,
+		borderBottomColor:'#aaa'
 
+
+
+	},
+	loginBtn:{
+		width:width*0.9,
+		height:35,
+		marginTop:30,
+		borderRadius:8,
+		justifyContent:'center',
+		alignItems:'center',
+		backgroundColor:'#1E90FF',
+		alignSelf:'center'
+	},
+	moreMenu:{
+		width:width*0.9,
+		flexDirection:'row',
+		justifyContent:'space-between',
+		alignSelf:'center',
+		marginTop:10
+	},
+	otherLogin:{
+		flexDirection:'row',
+		alignItems:'center',
+		marginBottom:20,
+		marginLeft:20
+	},
+	otherImges:{
+		borderRadius:40,
+		width:34,
+		height:34,
+		marginLeft:5
 	}
+
 })
